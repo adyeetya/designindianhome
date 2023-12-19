1cf875febc156b86ef5c2e3c8bf479d9526b3fdc
adyeetya <2612adityasingh2000@gmail.com>
2023-12-19 15:22:57 +0530

    Merge changes from origin/main

---
+++ components/EndToEndImageGrid/page.tsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import MaxWidthWrapper from '../MaxWidthWrapper'
+
+const EndToEndImageGrid = () => {
+  // Sample list of images with associated text
+  const imageList = [
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+    {
+      src: '/images/Structural Renovation.jpg',
+      text: 'Image 1',
+    },
+
+    // Add more images as needed
+  ]
+
+  return (
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div className="py-2">
+          <div className="container mx-auto text-center">
+            <h2 className="text-4xl font-bold mb-4">End To End Structural</h2>
+            <p className="text-gray-600">
+              End To End We Undertake Including Complete Renovations
+            </p>
+          </div>
+
+          <div className="container mx-auto mt-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+              {imageList.map((item, index) => (
+                <div key={index} className="text-center">
+                  <img
+                    src={item.src}
+                    alt={item.text}
+                    className="w-full  object-cover"
+                  />
+                  <p className="text-xs mt-2">{item.text}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </>
+  )
+}
+
+export default EndToEndImageGrid
