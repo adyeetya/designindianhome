43d5528d3ee5b29668629c4c853c5f318ab12728
swati <76866808+suvanipriyam@users.noreply.github.com>
2023-12-28 18:23:36 +0530

    home page section

--- components/Collection/page.jsx
+++ components/Collection/page.jsx
@@ -1,121 +1,127 @@
-const Collection = () =>{
-    return(
-        <>
-        <div className="containr">
-          <h1>
+const Collection = () => {
+  return (
+    <>
+      <div className="containr">
+        <h1>
           Why Design Indian Homes?
-          </h1>
+        </h1>
         <div id="content">
-  <div className="item">
-    
-      <img src="images/icon/icons8-on-time-delivery-53.png" />
-   
-    <p>
-      On time Delivery
-    </p>
-  </div>
-  
-  <div className="item">
-      <img src="images/icon/icons8-on-time-delivery-53.png"/>
-   
-    <p>
-      Lick butt cat snacks, so stick butt in face intently sniff hand, and
-      intrigued by the shower hunt anything that moves. Destroy couch as revenge
-      sit in box shove bum in owner's face like camera lens yet scratch leg;
-      meow for can opener to feed me if it fits, i sits. Steal the warm chair
-      right after you get up.
-    </p>
-  </div>
-  <div className="item">
-    <a href="http://placekitten.com/280/185">
-      <img src="http://placekitten.com/280/185" />
-    </a>
-    <p>
-      Get video posted to internet for chasing red dot play riveting piece on
-      synthesizer keyboard. Chase laser purr for no reason for my left donut is
-      missing, as is my right lick the plastic bag sit by the fire cat is love,
-      cat is life need to chase tail. Who's the baby has closed eyes but still
-      sees you for always hungry, favor packaging over toy. Eat from dog's food
-      brown cats with pink ears yet human give me attention meow chase mice.
-    </p>
-  </div>
-  <div className="item">
-    <a href="http://placekitten.com/280/215">
-      <img src="http://placekitten.com/280/215" />
-    </a>
-    <p>
-      Drink water out of the faucet hunt by meowing loudly at 5am next to human
-      slave food dispenser flop over, but the dog smells bad yet plan steps for
-      world domination or chase the pig around the house but need to chase tail.
-      Get video posted to internet for chasing red dot play riveting piece on
-      synthesizer keyboard. Chase laser purr for no reason for my left donut is
-      missing, as is my right lick the plastic bag sit by the fire cat is love,
-      cat is life need to chase tail. Who's the baby has closed eyes but still
-      sees you for always hungry, favor packaging over toy. Eat from dog's food
-      brown cats with pink ears yet human give me attention meow chase mice.{" "}
-    </p>
-  </div>
-  <div className="item">
-    <a href="http://placekitten.com/280/300">
-      <img src="http://placekitten.com/280/300" />
-    </a>
-    <p>
-      Unwrap toilet paper knock dish off table head butt cant eat out of my own
-      dish pooping rainbow while flying in a toasted bread costume in space but
-      spit up on light gray carpet instead of adjacent linoleum yet roll on the
-      floor purring your whiskers off. Brown cats with pink ears scamper but
-      present belly, scratch hand when stroked behind the couch, so jump launch
-      to pounce upon little yarn mouse, bare fangs at toy run hide in litter box
-      until treats are fed and hunt by meowing loudly at 5am next to human slave
-      food dispenser.{" "}
-    </p>
-  </div>
-  <div className="item">
-    <a href="http://placekitten.com/280/140">
-      <img src="http://placekitten.com/280/140" />
-    </a>
-    <p>
-      Stare at ceiling hack up furballs, have secret plans yet eat prawns
-      daintily with a claw then lick paws clean wash down prawns with a lap of
-      carnation milk then retire to the warmest spot on the couch to claw at the
-      fabric before taking a catnap or thug cat swat at dog tuxedo cats always
-      looking dapper. Wake up human for food at 4am spit up on light gray carpet
-      instead of adjacent linoleum but chew on cable put toy mouse in food bowl
-      run out of litter box at full speed fall over dead (not really but gets
-      sypathy) and thug cat . Hate dog plan steps for world domination always
-      hungry knock dish off table head butt cant eat out of my own dish yet
-      knock over christmas tree shove bum in owner's face like camera lens.{" "}
-    </p>
-  </div>
-  <div className="item">
-    <a href="http://placekitten.com/280/195">
-      <img src="http://placekitten.com/280/195" />
-    </a>
-    <p />
-    <p>
-      Claws in your leg jump around on couch, meow constantly until given food,
-      . Sit by the fire scratch leg; meow for can opener to feed me play
-      riveting piece on synthesizer keyboard but sweet beast cat is love, cat is
-      life. Eat and than sleep on your face unwrap toilet paper eat grass, throw
-      it back up has closed eyes but still sees you intently stare at the same
-      spot. Pee in the shoe.{" "}
-    </p>
-  </div>
-  
- 
-  <div className="foo" />
-  <div className="foo" />
-  <div className="foo" />
-  <div className="foo" />
-  <div className="foo" />
-  <div className="foo" />
-  <div className="foo" />
-  <div className="foo" />
-  <div className="foo" />
-</div>
-</div>
-        </>
-    )
+          <div className="item">
+
+            <img src="images/icon/icons8-on-time-delivery-53.png" />
+
+            <p>
+              On time Delivery
+            </p>
+          </div>
+
+          <div className="item">
+            <img src="images/icon/icons8-home-50.png" />
+
+            <p>
+              1609+ Happy Homes
+            </p>
+          </div>
+          <div className="item">
+            <img src="images/icon/icons8-structure-26.png" />
+
+            <p>
+              81+ Inhouse Architects & Interior Pro's
+            </p>
+          </div>
+          <div className="item">
+            <img src="images/icon/icons8-construction-building-96.png" />
+
+            <p>
+              End To End Interiors & Structure Building
+            </p>
+          </div>
+          <div className="item">
+            <img src="images/icon/icons8-warranty-64.png" />
+
+            <p>
+              Warranty Direct from Source
+            </p>
+          </div>
+          <div className="item">
+            <img src="images/icon/icons8-engineer-50.png" />
+
+            <p>
+              Professional Personnel
+            </p>
+          </div>
+          <div className="item">
+            <img src="images/icon/icons8-construction-building-96.png" />
+
+            <p />
+            <p>
+              In-House Modular Manufacturing
+            </p>
+          </div>
+
+          <div className="item">
+            <img src="images/icon/icons8-check-50.png" />
+
+            <p />
+            <p>
+              Multiple Quality Checks
+            </p>
+          </div>
+          <div className="foo" />
+          <div className="foo" />
+          <div className="foo" />
+          <div className="foo" />
+          <div className="foo" />
+          <div className="foo" />
+          <div className="foo" />
+          <div className="foo" />
+          <div className="foo" />
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="road-map-main">
+              <div class="road-map-wrapper">
+                <div class="road-map-circle">
+                  <span
+                    class="road-map-circle-text d-flex align-items-center justify-content-center"
+                  >text</span
+                  >
+                </div>
+                <div class="road-map-card">
+                  <h4 class="card-head">headding</h4>
+                  <p class="card-text">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s
+                  </p>
+                </div>
+              </div>
+              <div class="road-map-wrapper">
+                <div class="road-map-circle">
+                  <span
+                    class="road-map-circle-text d-flex align-items-center justify-content-center"
+                  >text</span
+                  >
+                </div>
+                <div class="road-map-card">
+                  <h4 class="card-head">headding</h4>
+                  <p class="card-text">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Collection
