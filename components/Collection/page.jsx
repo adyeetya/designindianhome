2dbab697fd682c4842b886e712af65a50892951d
swati <76866808+suvanipriyam@users.noreply.github.com>
2023-12-30 13:14:53 +0530

    all home changes done

--- components/Collection/page.jsx
+++ components/Collection/page.jsx
@@ -181,155 +181,89 @@ const Collection = () => {
         <h1>End-to-end interior solutions</h1>
       </div>
 
+ <div className="img-area pt-6">
+          <div className="single-img" id="name">
+            <img src="images/a.png" alt="" />
+            <p className="text-lg font-bold" id="rem">Modular Kitchen</p>
+            
 
-      {/*   <div className="containr">
-        <h1>
-          Why Design Indian Homes?
-        </h1>
-        <div id="content">
-          <div className="item">
+          </div>
+          <div className="single-img" id="name">
+            <img src="images/b.png" alt="" />
+            <p className="text-lg font-bold" id="rem">Storage and wardrobe</p>
+            
+          </div>
+          <div className="single-img" id="name">
+            <img src="images/c.png" alt="" />
+            <p className="text-lg font-bold" id="rem">Crockery Units</p>
+            
+          </div>
+          <div className="single-img" id="name">
+            <img src="images/d.png" alt="" />
+            <p className="text-lg font-bold" id="rem">Space Saving Furniture</p>
+            
+          </div>
+          <div className="single-img" id="name">
+            <img src="images/e.png" alt="" />    <p className="text-lg font-bold" id="rem">TV Units</p>
+            
+          </div>
+          <div className="single-img" id="name">
+            <img src="images/f.png" alt="" />    <p className="text-lg font-bold" id="rem">
 
-            <img src="images/icon/icons8-on-time-delivery-53.png" />
+Study Tables</p>
+            
+          </div>
+          <div className="single-img" id="name">
+            <img src="images/g.png" alt="" />  <p className="text-lg font-bold" id="rem">Lights
 
-            <p>
-              On time Delivery
-            </p>
+
+
+
+</p>
+            
+
+          </div>
+          <div className="single-img" id="name">
+            <img src="images/h.png" alt="" />
+            <p className="text-lg font-bold" id="rem">False Ceiling</p>
+            
+          </div>
+          <div className="single-img" id="name">
+            <img src="images/i.png" alt="" />    <p className="text-lg font-bold" id="rem">Wallpaper</p>
+            
           </div>
 
-          <div className="item">
-            <img src="images/icon/icons8-home-50.png" />
+          <div className="single-img" id="name">
+            <img src="images/j.png" alt="" />    <p className="text-lg font-bold" id="rem">Wall Paint</p>
+            
+          </div>  <div className="single-img" id="name">
+            <img src="images/k.png" alt="" />    <p className="text-lg font-bold" id="rem">Bathroom</p>
+            
+          </div>  <div className="single-img" id="name">
+            <img src="images/l.png" alt="" />    <p className="text-lg font-bold" id="rem">Pooja Unit</p>
+            
+          </div>  <div className="single-img" id="name">
+            <img src="images/m.png" alt="" />    <p className="text-lg font-bold" id="rem">Foyer Designs
 
-            <p>
-              1609+ Happy Homes
-            </p>
+</p>
+            
+          </div>  <div className="single-img" id="name">
+            <img src="images/n.png" alt="" />    <p className="text-lg font-bold" id="rem">Movable furniture</p>
+            
+          </div> 
+           <div className="single-img" id="name">
+            <img src="images/o.png" alt="" />    <p className="text-lg font-bold" id="rem">Kids Bedroom</p>
+            
           </div>
-          <div className="item">
-            <img src="images/icon/icons8-structure-26.png" />
-
-            <p>
-              81+ Inhouse Architects & Interior Pro's
-            </p>
+          <div className="single-img" id="name">
+            <img src="images/p.png" alt="" />    <p className="text-lg font-bold" id="rem">Bookshelves</p>
+            
           </div>
-          <div className="item">
-            <img src="images/icon/icons8-construction-building-96.png" />
+         </div>
 
-            <p>
-              End To End Interiors & Structure Building
-            </p>
-          </div>
-          <div className="item">
-            <img src="images/icon/icons8-warranty-64.png" />
-
-            <p>
-              Warranty Direct from Source
-            </p>
-          </div>
-          <div className="item">
-            <img src="images/icon/icons8-engineer-50.png" />
-
-            <p>
-              Professional Personnel
-            </p>
-          </div>
-          <div className="item">
-            <img src="images/icon/icons8-construction-building-96.png" />
-
-            <p />
-            <p>
-              In-House Modular Manufacturing
-            </p>
-          </div>
-
-          <div className="item">
-            <img src="images/icon/icons8-check-50.png" />
-
-            <p />
-            <p>
-              Multiple Quality Checks
-            </p>
-          </div>
+    
+   
         
-        </div>
-      </div> */}
-      <div class="container mx-auto px-20">
-
-        <section class="py-8 px-16">
-          <div class="flex flex-wrap -mx-4 -mb-8">
-            <div class="md:w-1/4 px-4 mb-8">
-              <img class="rounded shadow-md" src="images/a.png" alt="" />
-
-              <p className="text-md text-center mt-2" >Modular Kitchen</p>
-
-            </div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/b.jpg" alt="" />
-
-              <p className="text-md text-center mt-2" >Storage and wardrobe</p>
-            </div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/c.jpg" alt="" />
-              <p className="text-md text-center mt-2" >Crockery Units</p></div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/d.jpg" alt="" />
-
-              <p className="text-md text-center mt-2" >Space Saving Furniture</p></div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/e.jpg" alt="" />
-              <p className="text-md text-center mt-2" >TV Units</p></div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/f.jpg" alt="" />
-
-              <p className="text-md text-center mt-2" >Study Tables</p></div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/g.jpg" alt="" />
-
-              <p className="text-md text-center mt-2" >Lights</p></div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/h.jpg" alt="" />
-
-              <p className="text-md text-center mt-2" >False Ceiling</p></div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/i.jpg" alt="" />
-              <p className="text-md text-center mt-2" >Wallpaper
-
-              </p></div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt="" />
-
-              <p className="text-md text-center mt-2" >False Ceiling</p>
-            </div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt="" />
-
-              <p className="text-md text-center mt-2" >Bathroom</p>
-            </div>
-
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="images/icon/icons8-bathroom-64.png" alt="" />
-
-              <p className="text-md text-center mt-2">Pooja Unit</p>
-            </div>
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt="" />
-
-              <p className="text-md text-center mt-2" > Foyer Designs</p>
-            </div>
-
-
-
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt="" />
-
-              <p className="text-md text-center mt-2" >Movable furniture</p>
-            </div>
-
-
-
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt="" />
-
-              <p className="text-md text-center mt-2" >
-                Kids Bedroom</p>
-            </div>
-
-
-            <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt="" />
-
-              <p className="text-md text-center mt-2" >Bookshelves</p>
-            </div>
-
-
-          </div>
-        </section>
-      </div>
-
-
     </>
   )
 }
