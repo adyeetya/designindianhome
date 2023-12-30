5ecc8472f3f1b6cf6cadc84b2c23218dd14c3058
swati <76866808+suvanipriyam@users.noreply.github.com>
2023-12-30 16:33:14 +0530

    images opttimised

--- components/Collection/page.jsx
+++ components/Collection/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 const Collection = () => {
   return (
     <>
@@ -8,7 +9,7 @@ const Collection = () => {
         <div id="content">
           <div className="item">
 
-            <img src="images/icon/icons8-on-time-delivery-53.png" />
+            <Image src="images/icon/icons8-on-time-delivery-53.png" />
 
             <p>
               On time Delivery
@@ -16,42 +17,42 @@ const Collection = () => {
           </div>
 
           <div className="item">
-            <img src="images/icon/icons8-home-50.png" />
+            <Image src="images/icon/icons8-home-50.png" />
 
             <p>
               1609+ Happy Homes
             </p>
           </div>
           <div className="item">
-            <img src="images/icon/icons8-structure-26.png" />
+            <Image src="images/icon/icons8-structure-26.png" />
 
             <p>
               81+ Inhouse Architects & Interior Pro's
             </p>
           </div>
           <div className="item">
-            <img src="images/icon/icons8-construction-building-96.png" />
+            <Image src="images/icon/icons8-construction-building-96.png" />
 
             <p>
               End To End Interiors & Structure Building
             </p>
           </div>
           <div className="item">
-            <img src="images/icon/icons8-warranty-64.png" />
+            <Image src="images/icon/icons8-warranty-64.png" />
 
             <p>
               Warranty Direct from Source
             </p>
           </div>
           <div className="item">
-            <img src="images/icon/icons8-engineer-50.png" />
+            <Image src="images/icon/icons8-engineer-50.png" />
 
             <p>
               Professional Personnel
             </p>
           </div>
           <div className="item">
-            <img src="images/icon/icons8-construction-building-96.png" />
+            <Image src="images/icon/icons8-construction-building-96.png" />
 
             <p />
             <p>
@@ -60,7 +61,7 @@ const Collection = () => {
           </div>
 
           <div className="item">
-            <img src="images/icon/icons8-check-50.png" />
+            <Image src="images/icon/icons8-check-50.png" />
 
             <p />
             <p>
@@ -86,28 +87,28 @@ const Collection = () => {
         <h1 className='font-bold text-2xl'>Stay safe. Design virtually.</h1><br />
         {/* Feature 1 */}
         <div className="text-center">
-          <img src="images/icon/12.png" alt="" className="w-60 h-70 mx-auto" />
+          <Image src="images/icon/12.png" alt="" className="w-60 h-70 mx-auto" />
           <h1 className='font-bold text-center'>Contactless Experience</h1>
           <p className="text-sm">No stepping out. Design your home interiors from the safety and comfort of your home.</p>
         </div>
 
         {/* Feature 2 */}
         <div className="text-center mt-4 md:mt-0">
-          <img src="images/icon/5.png" alt="" className="w-60 h-70 mx-auto" />
+          <Image src="images/icon/5.png" alt="" className="w-60 h-70 mx-auto" />
           <h1 className='font-bold text-center'>Online Expertise</h1>
           <p className="text-sm">Connect with our 600+ designers virtually and explore designs online.</p>
         </div>
 
         {/* Feature 3 */}
         <div className="text-center mt-4 md:mt-0">
-          <img src="images/icon/1.png" alt="" className="w-60 h-70 mx-auto" />
+          <Image src="images/icon/1.png" alt="" className="w-60 h-70 mx-auto" />
           <h1 className='font-bold text-center'>Live 3D Designs</h1>
           <p className="text-sm">Explore life-like 3D designs online that are made for your floor plan.</p>
         </div>
 
         {/* Feature 4 */}
         <div className="text-center mt-4 md:mt-0">
-          <img src="images/icon/2.png" alt="" className="w-60 h-70 mx-auto" />
+          <Image src="images/icon/2.png" alt="" className="w-60 h-70 mx-auto" />
           <h1 className='font-bold text-center'>Instant Pricing</h1>
           <p className="text-sm">Enjoy complete price transparency and stay within budget.</p>
         </div>
@@ -120,56 +121,56 @@ const Collection = () => {
       <div className="wrapper">
         <h1>Explore Our Collection</h1>
         <h3>For Beautiful Homes!</h3>
-        <div className="img-area ">
-          <div className="single-img" id="name">
-            <img src="images/1.png" alt="" />
+        <div className="Image-area ">
+          <div className="single-Image" id="name">
+            <Image src="images/1.png" alt="" />
             <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
 
           </div>
-          <div className="single-img" id="name">
-            <img src="images/2.png" alt="" />
+          <div className="single-Image" id="name">
+            <Image src="images/2.png" alt="" />
             <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
           </div>
-          <div className="single-img" id="name">
-            <img src="images/3.png" alt="" />
+          <div className="single-Image" id="name">
+            <Image src="images/3.png" alt="" />
             <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
           </div>
-          <div className="single-img" id="name">
-            <img src="images/4.png" alt="" />
+          <div className="single-Image" id="name">
+            <Image src="images/4.png" alt="" />
             <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
           </div>
-          <div className="single-img" id="name">
-            <img src="images/78.png" alt="" />    <p className="text-lg font-bold" id="rem">3 Seater Sofas
+          <div className="single-Image" id="name">
+            <Image src="images/78.png" alt="" />    <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
           </div>
-          <div className="single-img" id="name">
-            <img src="images/56.png" alt="" />    <p className="text-lg font-bold" id="rem">3 Seater Sofas
+          <div className="single-Image" id="name">
+            <Image src="images/56.png" alt="" />    <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
           </div>
-          <div className="single-img" id="name">
-            <img src="images/1.png" alt="" />  <p className="text-lg font-bold" id="rem">3 Seater Sofas
+          <div className="single-Image" id="name">
+            <Image src="images/1.png" alt="" />  <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
 
           </div>
-          <div className="single-img" id="name">
-            <img src="images/2.png" alt="" />
+          <div className="single-Image" id="name">
+            <Image src="images/2.png" alt="" />
             <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
           </div>
-          <div className="single-img" id="name">
-            <img src="images/3.png" alt="" />    <p className="text-lg font-bold" id="rem">3 Seater Sofas
+          <div className="single-Image" id="name">
+            <Image src="images/3.png" alt="" />    <p className="text-lg font-bold" id="rem">3 Seater Sofas
               Plush</p>
             <p className="text-md " >Plush, Stylish & Relaxing</p>
           </div>
@@ -181,40 +182,40 @@ const Collection = () => {
         <h1>End-to-end interior solutions</h1>
       </div>
 
- <div className="img-area pt-6">
-          <div className="single-img" id="name">
-            <img src="images/a.png" alt="" />
+ <div className="Image-area pt-6">
+          <div className="single-Image" id="name">
+            <Image src="images/a.png" alt="" />
             <p className="text-lg font-bold" id="rem">Modular Kitchen</p>
             
 
           </div>
-          <div className="single-img" id="name">
-            <img src="images/b.png" alt="" />
+          <div className="single-Image" id="name">
+            <Image src="images/b.png" alt="" />
             <p className="text-lg font-bold" id="rem">Storage and wardrobe</p>
             
           </div>
-          <div className="single-img" id="name">
-            <img src="images/c.png" alt="" />
+          <div className="single-Image" id="name">
+            <Image src="images/c.png" alt="" />
             <p className="text-lg font-bold" id="rem">Crockery Units</p>
             
           </div>
-          <div className="single-img" id="name">
-            <img src="images/d.png" alt="" />
+          <div className="single-Image" id="name">
+            <Image src="images/d.png" alt="" />
             <p className="text-lg font-bold" id="rem">Space Saving Furniture</p>
             
           </div>
-          <div className="single-img" id="name">
-            <img src="images/e.png" alt="" />    <p className="text-lg font-bold" id="rem">TV Units</p>
+          <div className="single-Image" id="name">
+            <Image src="images/e.png" alt="" />    <p className="text-lg font-bold" id="rem">TV Units</p>
             
           </div>
-          <div className="single-img" id="name">
-            <img src="images/f.png" alt="" />    <p className="text-lg font-bold" id="rem">
+          <div className="single-Image" id="name">
+            <Image src="images/f.png" alt="" />    <p className="text-lg font-bold" id="rem">
 
 Study Tables</p>
             
           </div>
-          <div className="single-img" id="name">
-            <img src="images/g.png" alt="" />  <p className="text-lg font-bold" id="rem">Lights
+          <div className="single-Image" id="name">
+            <Image src="images/g.png" alt="" />  <p className="text-lg font-bold" id="rem">Lights
 
 
 
@@ -223,40 +224,40 @@ Study Tables</p>
             
 
           </div>
-          <div className="single-img" id="name">
-            <img src="images/h.png" alt="" />
+          <div className="single-Image" id="name">
+            <Image src="images/h.png" alt="" />
             <p className="text-lg font-bold" id="rem">False Ceiling</p>
             
           </div>
-          <div className="single-img" id="name">
-            <img src="images/i.png" alt="" />    <p className="text-lg font-bold" id="rem">Wallpaper</p>
+          <div className="single-Image" id="name">
+            <Image src="images/i.png" alt="" />    <p className="text-lg font-bold" id="rem">Wallpaper</p>
             
           </div>
 
-          <div className="single-img" id="name">
-            <img src="images/j.png" alt="" />    <p className="text-lg font-bold" id="rem">Wall Paint</p>
+          <div className="single-Image" id="name">
+            <Image src="images/j.png" alt="" />    <p className="text-lg font-bold" id="rem">Wall Paint</p>
             
-          </div>  <div className="single-img" id="name">
-            <img src="images/k.png" alt="" />    <p className="text-lg font-bold" id="rem">Bathroom</p>
+          </div>  <div className="single-Image" id="name">
+            <Image src="images/k.png" alt="" />    <p className="text-lg font-bold" id="rem">Bathroom</p>
             
-          </div>  <div className="single-img" id="name">
-            <img src="images/l.png" alt="" />    <p className="text-lg font-bold" id="rem">Pooja Unit</p>
+          </div>  <div className="single-Image" id="name">
+            <Image src="images/l.png" alt="" />    <p className="text-lg font-bold" id="rem">Pooja Unit</p>
             
-          </div>  <div className="single-img" id="name">
-            <img src="images/m.png" alt="" />    <p className="text-lg font-bold" id="rem">Foyer Designs
+          </div>  <div className="single-Image" id="name">
+            <Image src="images/m.png" alt="" />    <p className="text-lg font-bold" id="rem">Foyer Designs
 
 </p>
             
-          </div>  <div className="single-img" id="name">
-            <img src="images/n.png" alt="" />    <p className="text-lg font-bold" id="rem">Movable furniture</p>
+          </div>  <div className="single-Image" id="name">
+            <Image src="images/n.png" alt="" />    <p className="text-lg font-bold" id="rem">Movable furniture</p>
             
           </div> 
-           <div className="single-img" id="name">
-            <img src="images/o.png" alt="" />    <p className="text-lg font-bold" id="rem">Kids Bedroom</p>
+           <div className="single-Image" id="name">
+            <Image src="images/o.png" alt="" />    <p className="text-lg font-bold" id="rem">Kids Bedroom</p>
             
           </div>
-          <div className="single-img" id="name">
-            <img src="images/p.png" alt="" />    <p className="text-lg font-bold" id="rem">Bookshelves</p>
+          <div className="single-Image" id="name">
+            <Image src="images/p.png" alt="" />    <p className="text-lg font-bold" id="rem">Bookshelves</p>
             
           </div>
          </div>
@@ -281,7 +282,7 @@ Study Tables</p>
          <div className="slider">
   <div className="slide-track">
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
         height={100}
         width={250}
@@ -289,7 +290,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
         height={100}
         width={250}
@@ -297,7 +298,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
         height={100}
         width={250}
@@ -305,7 +306,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
         height={100}
         width={250}
@@ -313,7 +314,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
         height={100}
         width={250}
@@ -321,7 +322,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
         height={100}
         width={250}
@@ -329,7 +330,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
         height={100}
         width={250}
@@ -337,7 +338,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
         height={100}
         width={250}
@@ -345,7 +346,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
         height={100}
         width={250}
@@ -353,7 +354,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
         height={100}
         width={250}
@@ -361,7 +362,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
         height={100}
         width={250}
@@ -369,7 +370,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
         height={100}
         width={250}
@@ -377,7 +378,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
         height={100}
         width={250}
@@ -385,7 +386,7 @@ Study Tables</p>
       />
     </div>
     <div className="slide">
-      <img
+      <Image
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
         height={100}
         width={250}
