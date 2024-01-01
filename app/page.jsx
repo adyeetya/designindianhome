6a3094900aac3e550ade6f9246cc2a70a589d5d3
adyeetya <2612adityasingh2000@gmail.com>
2024-01-01 18:02:57 +0530

    aditya fixes

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ import Display from '../components/Display/page'
 import EndToEndImageGrid from '../components/EndToEndImageGrid/page'
 import Stepper from '../components/Stepper/page'
 import FAQ from '../components/FAQ/page'
-import Collection from '@/components/Collection/page'
+import Collection from '../components/Collection/page'
 import '../style/hero.css'
 
 export default function Home() {
