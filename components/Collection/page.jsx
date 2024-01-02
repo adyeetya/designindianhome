31b755a7d438eb53c0e601f6193abe417d377866
swati <76866808+suvanipriyam@users.noreply.github.com>
2024-01-02 18:16:22 +0530

    .

--- components/Collection/page.jsx
+++ components/Collection/page.jsx
@@ -2,7 +2,102 @@ import Image from 'next/image'
 const Collection = () => {
   return (
     <>
+  <div className="container mx-auto px-4">
   
+  <section className="py-8 px-8">
+    <div className="flex flex-wrap -mx-4">
+      <div className="md:w-1/2 px-4 mb-8 md:mb-0">
+        <Image height={1000} width={1000}
+          className="rounded shadow-md"
+          src="/images/just sold.png"
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/2 px-4 mb-8 md:mb-0">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+    </div>
+  </section>
+ {/*  <section className="pt-8 px-4">
+    <div className="flex flex-wrap -mx-4">
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+    </div>
+  </section> */}
+  <section className="py-8 px-8">
+    <div className="flex flex-wrap -mx-4 -mb-8">
+     
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="/images/design1."
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/3 px-4 mb-8">
+        <img
+          className="rounded shadow-md"
+          src="https://source.unsplash.com/random/1280x720"
+          alt=""
+        />
+      </div>
+      
+    
+    </div>
+  </section>
+</div>
+
 
       <div className="wrapper">
         <h1>Explore Our Collection</h1>
