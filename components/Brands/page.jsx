cdb0d8a59d0a9ceabbf03b5ef82ee403b55579cc
swati <76866808+suvanipriyam@users.noreply.github.com>
2024-01-02 10:27:48 +0530

    Revert "."

--- components/Brands/page.jsx
+++ components/Brands/page.jsx
@@ -2,146 +2,91 @@ import Image from 'next/image'
 const Brands = () => {
   return (
     <>
-      <div className="wrapperlatest">
-        <h1>Package Offers</h1>
-        <h3>What People Say About Us</h3></div>
-      <div className="jiu">
-        <div className="containeru">
-          <div className="blog-post">
-            <img
-              src='https://source.unsplash.com/gray-2-seat-sofa-near-round-table-WPqcd-gpJeE' alt="Man"
-            />
-        
-            <div className="text-content">
-             
-              <h2 className="post-title">
-              Affordable, Yet Wholesome   </h2>
-              <h3>On Top Priority</h3>
-            <h3>  OFFER
-₹
-10.12
-Lac <span>
-₹
-6.8
-Lac*</span> ESSENTIAL WOOD WORKS FOR A 3BHK</h3>
-              <div className="tags">
-                <a href="#" target="_blank">
-                  #design
-                </a>
-                <a href="#" target="_blank">
-                  #photo
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="blog-post">
-            <img
-              src="https://source.unsplash.com/clear-glass-top-table-with-brown-wooden-base-YAp3j8EcjUo "
-              alt="Man"
-            />
-            
-            <div className="text-content">
-            
-              <h2 className="post-title">
-              Pocket Friendly, Yet Welcoming
-              </h2>
-              <div className="tags">
-                <a href="#" target="_blank">
-                  #design
-                </a>
-                <a href="#" target="_blank">
-                  #photo
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="blog-post">
-            <img
-              src="https://source.unsplash.com/black-metal-3-tier-rack-beside-white-couch-L5L2hiSh2Bo" black-metal-3-tier-rack-beside-white-couch-L5L2hiSh2Bo
-              alt="Man"
-            />
-          
-            <div className="text-content">
-             
-              <h2 className="post-title">
-              Top Notch Luxury, With Top Notch Pricing     </h2>
-              <div className="tags">
-                <a href="#" target="_blank">
-                  #design
-                </a>
-                <a href="#" target="_blank">
-                  #photo
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="blog-post">
-            <img
-              src="https://source.unsplash.com/brown-wooden-armchair-beside-brown-leather-couch-irPhJCHpZyo" black-metal-3-tier-rack-beside-white-couch-L5L2hiSh2Bo
-              alt="Man"
-            />
-         
-            <div className="text-content">
-             
-              <h2 className="post-title">
-              High Quality with Complete Commitment    </h2>
-              <div className="tags">
-                <a href="#" target="_blank">
-                  #design
-                </a>
-                <a href="#" target="_blank">
-                  #photo
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="blog-post">
-            <img
-              src="https://source.unsplash.com/a-living-room-with-a-couch-coffee-table-and-potted-plant-Rf0a8n0VV_M" black-metal-3-tier-rack-beside-white-couch-L5L2hiSh2Bo
-              alt="Man"
-            />
-
-            <div className="text-content">
-             
-              <h2 className="post-title">
-              Minimal, Yet Overall Coverage
-              </h2>
-              <div className="tags">
-                <a href="#" target="_blank">
-                  #design
-                </a>
-                <a href="#" target="_blank">
-                  #photo
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="blog-post">
-            <img
-              src="https://source.unsplash.com/white-wooden-coffee-table-near-white-sofa-WgkA3CSFrjc" black-metal-3-tier-rack-beside-white-couch-L5L2hiSh2Bo
-              alt="Man"
-            />
-          
-            <div className="text-content">
-             
-              <h2 className="post-title">
-              Luxury, Yet Affordable     </h2>
-              <div className="tags">
-                <a href="#" target="_blank">
-                  #design
-                </a>
-                <a href="#" target="_blank">
-                  #photo
-                </a>
-              </div>
-            </div>
-          </div>
-
-        </div>
+    <div className="jiu">
+    <div className="containeru">
+  <div className="blog-post">
+    <img
+      src="https://images.unsplash.com/photo-1629941181163-cdfb4bce340c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+      alt="Man"
+    />
+    <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank" className="category">
+      Insparation
+    </a>
+    <div className="text-content">
+      <img
+        src="https://images.unsplash.com/photo-1485893226355-9a1c32a0c81e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+        alt=""
+      />
+      <h2 className="post-title">
+        21 Photos That Might Make You Laugh For Once...
+      </h2>
+      <div className="tags">
+        <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank">
+          #design
+        </a>
+        <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank">
+          #photo
+        </a>
       </div>
+    </div>
+  </div>
+  <div className="blog-post">
+    <img
+      src="https://images.unsplash.com/photo-1629941181163-cdfb4bce340c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+      alt="Man"
+    />
+    <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank" className="category">
+      Insparation
+    </a>
+    <div className="text-content">
+      <img
+        src="https://images.unsplash.com/photo-1485893226355-9a1c32a0c81e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+        alt=""
+      />
+      <h2 className="post-title">
+        21 Photos That Might Make You Laugh For Once...
+      </h2>
+      <div className="tags">
+        <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank">
+          #design
+        </a>
+        <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank">
+          #photo
+        </a>
+      </div>
+    </div>
+  </div>
+  <div className="blog-post">
+    <img
+      src="https://images.unsplash.com/photo-1629941181163-cdfb4bce340c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+      alt="Man"
+    />
+    <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank" className="category">
+      Insparation
+    </a>
+    <div className="text-content">
+      <img
+        src="https://images.unsplash.com/photo-1485893226355-9a1c32a0c81e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+        alt=""
+      />
+      <h2 className="post-title">
+        21 Photos That Might Make You Laugh For Once...
+      </h2>
+      <div className="tags">
+        <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank">
+          #design
+        </a>
+        <a href="https://youtu.be/SdNaoYS_6Fo" target="_blank">
+          #photo
+        </a>
+      </div>
+    </div>
+  </div>
+  
+</div>
+</div>
 
 
     </>
-  )
-}
-export default Brands
+    )}
+    export default Brands
