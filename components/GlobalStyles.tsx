d739da5c01f182915402101eadc12892a28a726b
Abhishek kumar <41527391+Invens@users.noreply.github.com>
2024-01-03 07:40:54 +0000

    final changes

---
+++ components/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+// globalStyles.tsx
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=BioRhyme:wght@400;700&display=swap');
+
+  body {
+    font-family: 'BioRhyme', serif;
+    /* Additional global styles can be added here */
+  }
+`;
