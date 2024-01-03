4aab883c6d1c2d91bfb7c9496a3e9e446e4ad401
adyeetya <2612adityasingh2000@gmail.com>
2024-01-03 16:37:40 +0530

    minor

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
-"use client";
+'use client'
 import Hero from '../components/Hero/page'
-import { Fragment } from 'react';
+import { Fragment } from 'react'
 import ImageGrid from '../components/ImageGrid/page'
 import Image from 'next/image'
 import Brands from '../components/Brands/page'
@@ -16,33 +16,26 @@ import MyForm from '../components/MyForm'
 import '../style/hero.css'
 import Omsairam from '../components/Navbar/Omsairam'
 
-
-
-
 export default function Home() {
   return (
     <>
-    <Fragment>
+      <Fragment>
+        <Omsairam />
+        <Header />
+        <Hero />
+        <ImageGrid />
+        <Collection />
+        <Brands />
+        <Display />
+        <Stepper />
+        <TabsSection />
+        <EndToEndImageGrid />
+        <FAQ />
+        <div className="bg-amber-50 p-4">
+          <MyForm />
+        </div>
 
-    
-   
-
-      <Omsairam />
-      <Header />
-      <Hero />
-      <ImageGrid />
-      <Collection />
-      <Brands />
-      <Display />
-      <Stepper />
-      <TabsSection />
-      <EndToEndImageGrid />
-      <FAQ />
-      <div className="bg-amber-50 p-4">
-        <MyForm />
-      </div>
-
-      <Footer />
+        <Footer />
       </Fragment>
     </>
   )
