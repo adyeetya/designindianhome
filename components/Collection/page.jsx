82731c416a790df1b4dbd125b5a6de1286f7d4a9
swati <76866808+suvanipriyam@users.noreply.github.com>
2024-01-08 14:46:54 +0530

    .

--- components/Collection/page.jsx
+++ components/Collection/page.jsx
@@ -763,7 +763,9 @@ const Collection = () => {
             </div>
           );
         })}
-      </Carousel> <div className="flex justify-center items-center "><button className=' bg-green-700 text-lg py-3 px-3 mb-12 rounded-full text-white  font-bold '>Check Out More From Our Collections</button>
+      </Carousel>
+      
+       <div className="flex justify-center items-center "><button className=' bg-green-700 text-lg py-3 px-3 mb-12 rounded-full text-white  font-bold '>Check Out More From Our Collections</button>
      </div> 
     </div>
 
